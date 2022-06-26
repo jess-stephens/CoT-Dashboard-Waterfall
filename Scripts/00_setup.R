77cc2c5331a068b93d5913895003a89a262b3a15
Jessica Stephens <64651019+jess-stephens@users.noreply.github.com>
2022-06-26 04:32:52 -0400

    Created refresh and QC files

--- Scripts/00_setup.R
+++ Scripts/00_setup.R
@@ -1,6 +1,20 @@
+######## ONLY RUN THIS SCRIPT THE FIRST TIME USING THIS CODE ON YOUR COMPUTERS #####
+
+# Project: Uganda CoT Interagency Dashboard
+# Script: 00_setup
+# Developers: Alex Brun (DOD), Jessica Stephens (USAID)
+# Use: To integrate Uganda MoH/METS quarterly data into ICPI/CoT CoOP Continuity of Treatment
+# Dashboard, in advance of the DATIM release to encourage more timely data usage. 
+ 
+
+# Instructions:
+
 # Install packages
-install.packages(c("openxlsx", "reshape2", "tidyverse", "readxl", "data.table", "usethis"))
+install.packages(c("tidyverse","openxlsx", "readxl","reshape2", "data.table", "usethis"))
 
 # Create standard folder structure
 library(glamr)
 si_setup()
+
+# Add raw data to the Data folder
+# This step needs to be done physically, outside of R
